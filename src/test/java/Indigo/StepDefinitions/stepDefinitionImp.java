7dbca552d5d8bb43993d807c39017d6fffee9f67
Deepak Sihare <deepaksihare891@gmail.com>
2023-05-07 17:25:50 +0530

    Configured email

--- src/test/java/Indigo/StepDefinitions/stepDefinitionImp.java
+++ src/test/java/Indigo/StepDefinitions/stepDefinitionImp.java
@@ -1,6 +1,7 @@
 package Indigo.StepDefinitions;
 
 import Indigo.Components.BaseTest;
+import Indigo.Components.emailSend;
 import Indigo.PageObjects.ConfirmationPage;
 import Indigo.PageObjects.FlightCatalogue;
 import Indigo.PageObjects.LandingPage;
@@ -15,19 +16,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import java.io.File;
 import java.io.IOException;
 
-import java.sql.SQLOutput;
-import java.util.Properties;
 
 public class stepDefinitionImp extends BaseTest {
 
@@ -331,67 +322,5 @@ public class stepDefinitionImp extends BaseTest {
         }
         System.out.println("browser close");
         driver.quit();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Send email");
-        emailSend("Please find the attached report of Sanity suite");
-    }
-
-    public void emailSend(String error){
-
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("deepaksihare891@gmail.com", "abpgdenmkmbmaabg");
-                    } });
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new
-                    InternetAddress("deepaksihare891@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse("deepaksihare891@gmail.com"));
-            message.setSubject("Indigo Sanity Report");
-
-            BodyPart messageBodyPart1 = new MimeBodyPart();
-
-            messageBodyPart1.setText(error);
-
-            MimeBodyPart messageBodyPart2 = new MimeBodyPart();
-
-            String filename = System.getProperty("user.dir") +
-                    "/Reports/Spark.html";
-
-            DataSource source = new FileDataSource(filename);
-
-            messageBodyPart2.setDataHandler(new DataHandler(source));
-
-            // set the file
-            messageBodyPart2.setFileName(filename);
-
-            Multipart multipart = new MimeMultipart();
-            multipart.addBodyPart(messageBodyPart2);
-            multipart.addBodyPart(messageBodyPart1);
-            message.setContent(multipart);
-            Transport.send(message);
-
-            System.out.println("=====Email Sent=====");
-
-        } catch (MessagingException e) {
-
-            throw new RuntimeException(e);
-
-        }
     }
 }
